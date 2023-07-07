54c526bf3212f0b74d0aafc6cf4e4e10ef5162bc
SeongrokKim <tjdfhr1228@gmail.com>
2023-07-07 10:48:07 +0900

    add kakao login

---
+++ app/src/main/java/com/example/madcampweek2/LoginActivity.java
@@ -0,0 +1,111 @@
+package com.example.madcampweek2;
+
+import static android.content.ContentValues.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.User;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function2;
+
+public class LoginActivity extends AppCompatActivity {
+    private Button loginButton;
+    private EditText id;
+    private EditText password;
+    private Button joinButton;
+    private View loginKakao;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.login_button);
+        id = findViewById(R.id.login_id);
+        password = findViewById(R.id.login_password);
+        joinButton = findViewById(R.id.join_button);
+        loginKakao = findViewById(R.id.login_kakao);
+
+        Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
+            @Override
+            public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
+                UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+                    @Override
+                    public Unit invoke(User user, Throwable throwable) {
+                        if (user != null){
+                            Log.d(TAG, "invoke: id" + user.getId());
+                        } else{
+                            Log.d(TAG, "로그인 안되어 있네");
+                        }
+                        return null;
+                    }
+                });
+                return null;
+            }
+        };
+
+        joinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+        loginKakao.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(LoginActivity.this)) {
+                    UserApiClient.getInstance().loginWithKakaoTalk(LoginActivity.this, callback);
+                }else {
+                    UserApiClient.getInstance().loginWithKakaoAccount(LoginActivity.this, callback);
+                }
+            }
+        });
+
+        updateKakaoLoginUi();
+
+    }
+
+    private  void updateKakaoLoginUi(){
+        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+            @Override
+            public Unit invoke(User user, Throwable throwable) {
+                // 로그인이 되어있으면
+                if (user!=null){
+
+                    // 유저의 아이디
+                    Log.d(TAG,"invoke: id" + user.getId());
+                    // 유저의 어카운트정보에 이메일
+                    Log.d(TAG,"invoke: nickname" + user.getKakaoAccount().getEmail());
+                    // 유저의 어카운트 정보의 프로파일에 닉네임
+                    Log.d(TAG,"invoke: email" + user.getKakaoAccount().getProfile().getNickname());
+                    // 유저의 어카운트 파일의 성별
+                    Log.d(TAG,"invoke: gerder" + user.getKakaoAccount().getGender());
+                    // 유저의 어카운트 정보에 나이
+                    Log.d(TAG,"invoke: age" + user.getKakaoAccount().getAgeRange());
+
+                    loginKakao.setVisibility(View.GONE);
+                }else {
+                    // 로그인이 되어 있지 않다면 위와 반대로
+                }
+                return null;
+            }
+        });
+    }
+}
