15f032383dc5f862fc70915fb5c7adad128de7fe
SeongrokKim <tjdfhr1228@gmail.com>
2023-07-07 20:12:08 +0900

    add google login

--- app/src/main/java/com/example/madcampweek2/LoginActivity.java
+++ app/src/main/java/com/example/madcampweek2/LoginActivity.java
@@ -2,19 +2,36 @@ package com.example.madcampweek2;
 
 import static android.content.ContentValues.TAG;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.IntentSenderRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.auth.api.identity.GetSignInIntentRequest;
+import com.google.android.gms.auth.api.identity.Identity;
+import com.google.android.gms.auth.api.identity.SignInCredential;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
 import com.kakao.sdk.auth.model.OAuthToken;
 import com.kakao.sdk.user.UserApiClient;
 import com.kakao.sdk.user.model.User;
@@ -31,6 +48,39 @@ public class LoginActivity extends AppCompatActivity {
     private EditText password;
     private Button joinButton;
     private View loginKakao;
+    private View loginGoogle;
+    private View loginNaver;
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google 로그인에 성공한 경우
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                // 사용자 이름과 이미지를 가져옵니다.
+                String name = account.getDisplayName();
+                Uri photoUri = account.getPhotoUrl();
+
+                // MainActivity로 이동하기 위한 Intent를 생성합니다.
+                Intent intent = new Intent(this, MainActivity.class);
+                // 사용자 이름과 이미지를 Intent에 추가합니다.
+                intent.putExtra("name", name);
+                intent.putExtra("photoUri", String.valueOf(photoUri));
+
+                // MainActivity로 이동합니다.
+                startActivity(intent);
+            } catch (ApiException e) {
+                // Google 로그인에 실패한 경우
+                Toast.makeText(getApplicationContext(),"Google Sign-in failed", Toast.LENGTH_SHORT).show();
+                // 실패 처리를 수행합니다.
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,24 +91,31 @@ public class LoginActivity extends AppCompatActivity {
         password = findViewById(R.id.login_password);
         joinButton = findViewById(R.id.join_button);
         loginKakao = findViewById(R.id.login_kakao);
+        loginGoogle = findViewById(R.id.login_google);
+        loginNaver = findViewById(R.id.login_naver);
 
         Function2<OAuthToken, Throwable, Unit> callback = new Function2<OAuthToken, Throwable, Unit>() {
             @Override
             public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
-                UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
-                    @Override
-                    public Unit invoke(User user, Throwable throwable) {
-                        if (user != null){
-                            Log.d(TAG, "invoke: id" + user.getId());
-                        } else{
-                            Log.d(TAG, "로그인 안되어 있네");
-                        }
-                        return null;
-                    }
-                });
+                //oAuthToken != null 이라면 로그인 성공
+                if(oAuthToken!=null){
+                    // 토큰이 전달된다면 로그인이 성공한 것이고 토큰이 전달되지 않으면 로그인 실패한다.
+                    updateKakaoLoginUi();
+
+                }else {
+                    //로그인 실패
+                    Toast.makeText(getApplicationContext(), "로그인 실패", Toast.LENGTH_SHORT).show();
+                }
                 return null;
             }
         };
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "로그인 하는 중", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         joinButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,9 +133,34 @@ public class LoginActivity extends AppCompatActivity {
                     UserApiClient.getInstance().loginWithKakaoAccount(LoginActivity.this, callback);
                 }
             }
+
         });
 
-        updateKakaoLoginUi();
+        loginGoogle.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .requestProfile()
+                        .build();
+
+                GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(LoginActivity.this, gso);
+
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+
+        });
+
+        loginNaver.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+            }
+
+        });
+
+
 
     }
 
@@ -88,19 +170,10 @@ public class LoginActivity extends AppCompatActivity {
             public Unit invoke(User user, Throwable throwable) {
                 // 로그인이 되어있으면
                 if (user!=null){
-
-                    // 유저의 아이디
-                    Log.d(TAG,"invoke: id" + user.getId());
-                    // 유저의 어카운트정보에 이메일
-                    Log.d(TAG,"invoke: nickname" + user.getKakaoAccount().getEmail());
-                    // 유저의 어카운트 정보의 프로파일에 닉네임
-                    Log.d(TAG,"invoke: email" + user.getKakaoAccount().getProfile().getNickname());
-                    // 유저의 어카운트 파일의 성별
-                    Log.d(TAG,"invoke: gerder" + user.getKakaoAccount().getGender());
-                    // 유저의 어카운트 정보에 나이
-                    Log.d(TAG,"invoke: age" + user.getKakaoAccount().getAgeRange());
-
-                    loginKakao.setVisibility(View.GONE);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.putExtra("name", user.getKakaoAccount().getProfile().getNickname());
+                    intent.putExtra("photoUri", Uri.parse(user.getKakaoAccount().getProfile().getProfileImageUrl()));
+                    startActivity(intent);
                 }else {
                     // 로그인이 되어 있지 않다면 위와 반대로
                 }
--- app/src/main/java/com/example/madcampweek2/RegisterActivity.java
+++ app/src/main/java/com/example/madcampweek2/RegisterActivity.java
@@ -1,41 +1,18 @@
 package com.example.madcampweek2;
 
-import static android.content.ContentValues.TAG;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.kakao.sdk.auth.model.OAuthToken;
-import com.kakao.sdk.user.UserApi;
-import com.kakao.sdk.user.UserApiClient;
-import com.kakao.sdk.user.model.User;
-
-import kotlin.Unit;
-import kotlin.jvm.functions.Function2;
 
 public class RegisterActivity extends AppCompatActivity {
-    private Button btnMakeAccount;
-    private Button btnKakao;
-    private Button btnGoogle;
-    private Button btnNaver;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-
-
-        btnMakeAccount = findViewById(R.id.btnMakeAccount);
-        btnKakao = findViewById(R.id.btnKakao);
-        btnGoogle = findViewById(R.id.btnGoogle);
-        btnNaver = findViewById(R.id.btnNaver);
     }
 
 
